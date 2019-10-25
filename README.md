11e66be6722116d43dcbf5400f2f7839de2dc014
George Campbell <campbell.goe@gmail.com>
2019-10-25 19:00:33 +0100

    Minor improvements and general set up

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ This repository hosts the code for the documentation of the Robomator (project n
 
 This project was bootstrapped using `npx docusaurus-init .`
 
-See: https://docusaurus.io/
+Check the [documentation](https://docusaurus.io) for how to use Docusaurus.
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,17 +21,17 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  title: 'Robomator (name to be determined)', // Title for your website.
+  tagline: 'Documentation website for projects made by the javascript project facebook group.',
+  url: 'https://example.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'Robomator',
+  organizationName: 'open_source',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -54,8 +54,8 @@ const siteConfig = {
 
   /* Colors for website */
   colors: {
-    primaryColor: '#806208',
-    secondaryColor: '#594405',
+    primaryColor: '#8946c4',
+    secondaryColor: '#bb96dc',
   },
 
   /* Custom fonts for website */
